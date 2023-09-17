fd91c07e6f577d25a1c8695363ed735fae31bad0
rasulomaroff <rasulomaroff@gmail.com>
2023-09-17 12:20:13 +0300

    fix(highlight): fixes a bug when highlight weren't correctly applied when entering initially an empty buffer

--- plugin/reactive.lua
+++ plugin/reactive.lua
@@ -11,15 +11,11 @@ local group = api.nvim_create_augroup('reactive.nvim', { clear = true })
 local Highlight = require 'reactive.highlight'
 local Snapshot = require 'reactive.snapshot'
 
-local last_event
-
 aucmd('ModeChanged', {
   group = group,
   pattern = '*:*',
   desc = 'Reactive: watches for mode changes to update highlights and run callbacks',
   callback = function(opts)
-    last_event = opts.event
-
     local from, to = unpack(vim.split(opts.match, ':'))
 
     Snapshot:set_modes(from, to)
@@ -32,23 +28,16 @@ aucmd('ModeChanged', {
 -- then through telescope go to another one and then go back through Ctrl + o
 aucmd({ 'WinEnter', 'BufWinEnter' }, {
   group = group,
-  desc = 'Reactive: applies highlights when entering a window',
-  callback = function(opts)
-    -- BufWinEnter event is often triggered right after `WinEnter` event,
-    -- in this case we don't need to do the same work twice
-    if last_event ~= 'WinEnter' or opts.event ~= 'BufWinEnter' then
-      Highlight:apply(Snapshot:gen())
-    end
-
-    last_event = opts.event
+  desc = 'Reactive: applies active window highlights',
+  callback = function()
+    Highlight:apply(Snapshot:gen())
   end,
 })
 
 aucmd('WinLeave', {
   group = group,
-  desc = 'Reactive: removes highlights when leaving a window',
-  callback = function(opts)
-    last_event = opts.event
+  desc = 'Reactive: applies inactive window highlights',
+  callback = function()
     Highlight:apply(Snapshot:gen(true))
   end,
 })
